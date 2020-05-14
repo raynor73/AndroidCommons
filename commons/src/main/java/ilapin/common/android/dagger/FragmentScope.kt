ec099ff122249cf185893287a721f309f4419fc8
Igor Lapin <raynor73@gmail.com>
2020-05-14 23:46:48 +0100

    Add dagger scopes

---
+++ commons/src/main/java/ilapin/common/android/dagger/FragmentScope.kt
@@ -0,0 +1,10 @@
+package ilapin.common.android.dagger
+
+import javax.inject.Scope
+
+/**
+ * @author igorlapin on 14/05/2020.
+ */
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class FragmentScope
